93b63635a42c118b83d9493a0928454e12d79086
lwx457280 <li448341911@qq.com>
2020-01-05 21:20:06 +0800

    【线程并发模拟演示】

---
+++ concurrency/src/main/java/com/example/concurrency/count/CountExample4.java
@@ -0,0 +1,51 @@
+package com.example.concurrency.count;
+
+import com.example.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class CountExample4 {
+    /**
+     * 请求总数
+     */
+    private static int clientTotal = 5000;
+    // 同时并发执行线程数
+    private static int threadTotal = 200;
+
+    private static volatile int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int index = 0; index < clientTotal; index++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("Exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        // 线程执行完之后，如果这个线程池不再使用需要关闭
+        executorService.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void add() {
+        count++;
+        // 1、count
+        // 2、+1
+        // 3、count
+    }
+}
