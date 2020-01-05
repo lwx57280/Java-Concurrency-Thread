fa8701607d646a44bbf996886260db3215263a58
lwx457280 <li448341911@qq.com>
2020-01-05 21:18:37 +0800

    【线程封闭】

--- chapter06/src/main/java/com/example/threadconcurrency/threadlocal/RequestHolder.java
+++ chapter06/src/main/java/com/example/threadconcurrency/threadlocal/RequestHolder.java
@@ -1,5 +1,13 @@
 package com.example.threadconcurrency.threadlocal;
 
+/**
+ * 线程封闭
+ *
+ * ad-hoc线程封闭：程序控制实现，最糟糕，忽略
+ * 堆栈封闭：局部变量、无并发问题
+ *
+ * ThreadLocal 线程封闭：特别好的封闭方法
+ */
 public class RequestHolder {
 
     private final static ThreadLocal<Long> requestHolder = new ThreadLocal<>();
