1cd75bd775b249af06fb3f6d8074e85fdc0acc17
lwx457280 <li448341911@qq.com>
2020-01-05 16:34:12 +0800

    【线程安全策略】

---
+++ chapter06/src/main/java/com/example/threadconcurrency/ThreadConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.threadconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadConcurrencyApplication.class, args);
+    }
+
+}
