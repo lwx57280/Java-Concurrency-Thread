0756fe17fe2fe6953d82602865b8c53eebea563a
licongzhi <li448341911@qq.com>
2022-09-17 15:51:03 +0800

    【volatile 线程可见性】

--- volatile-example/src/main/java/com/concurrent/cacheline/VolatileExample.java
+++ volatile-example/src/main/java/com/concurrent/cacheline/VolatileExample.java
@@ -1,20 +1,21 @@
 package com.concurrent.cacheline;
+
 /**
- * @description:    volatile 线程可见性
- * @author:         cong zhi
- * @createDate:     2022/5/30 11:16
- * @updateUser:     cong zhi
- * @updateDate:     2022/5/30 11:16
- * @updateRemark:   修改内容
- * @version:        v1.0
+ * @description: volatile 线程可见性
+ * @author: cong zhi
+ * @createDate: 2022/5/30 11:16
+ * @updateUser: cong zhi
+ * @updateDate: 2022/5/30 11:16
+ * @updateRemark: 修改内容
+ * @version: v1.0
  */
 public class VolatileExample {
 
-    static volatile boolean stop=false;
+    public static volatile boolean stop = false;
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
-            int i=0;
+            int i = 0;
             // 活性失效
             while (!stop) {
                 i++;
